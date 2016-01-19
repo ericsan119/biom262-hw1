c23890c3fe87362efafb23c0cc9b1bbe4bb7f151
LElmen <elmen@sbp.edu>
2016-01-19 11:39:32 -0800

    Lisa exercises 4 and 5

--- tf_binding.sh
+++ tf_binding.sh
@@ -7,10 +7,15 @@ awk '{if($3 == "transcript") print}' gencode.v19.annotation.chr22.gtf > gencode.
 exercise 3:
 module load biotools
 bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -l 2000 -r -0 -s > gencode.v19.annotation.chr22.transcript.promoter.gtf
-Lisa Elmen has done exercise 4, 5
+Lisa Elmen has done exercise 4 and 5
+exercise 4:
 module load biotools
 bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed  > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
+exercise 5:
 module load biotools
 bedtools getfasta -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta 
+
+
+
 
 
