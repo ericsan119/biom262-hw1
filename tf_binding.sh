5a7a7f1a07d2259aefb51b7c3a5d0e2a084109a3
LElmen <elmen@sbp.edu>
2016-01-19 11:53:40 -0800

    Update tf_binding.sh
    Added #-signs since these are comments and not part of script.

--- tf_binding.sh
+++ tf_binding.sh
@@ -1,18 +1,16 @@
-Chin San, Loo has done excersice 1, 2 and 3.
+#Chin San, Loo has done excersice 1, 2 and 3.
 The code as below:
-exercise 1:
+#exercise 1:
 grep 'NFKB' tf.bed > tf.nfkb.bed
-exercise 2:
+#exercise 2:
 awk '{if($3 == "transcript") print}' gencode.v19.annotation.chr22.gtf > gencode.v19.annotation.chr22.transcript.gtf
-exercise 3:
+#exercise 3:
 module load biotools
 bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -l 2000 -r -0 -s > gencode.v19.annotation.chr22.transcript.promoter.gtf
-Lisa Elmen has done exercise 4 and 5
-exercise 4:
-module load biotools
+#Lisa Elmen has done exercise 4 and 5
+#exercise 4:
 bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed  > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
-exercise 5:
-module load biotools
+#exercise 5:
 bedtools getfasta -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta 
 
 
