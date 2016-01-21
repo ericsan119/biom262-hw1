061085a830fb415f7c28c109ba74b8b398b5b174
ericsan119 <csloo@ucsd.edu>
2016-01-21 12:56:53 -0800

    Update tf_binding.sh

--- tf_binding.sh
+++ tf_binding.sh
@@ -20,6 +20,22 @@ bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf
 bedtools getfasta -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta 
 #exercise 6:
 #More work is needed to “validate” the homework fasta-file. I expected to find some matches using the data from Wong et al, but came up with 0 matches which does not seem reasonable. I tried different variants of grep to compare the list of canonical NFKB sequences, and the non-canonical consensus sequences with commands like “grep -Fx -f NFKBcanonical gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta  > Hope_to_find_match.txt” OR “grep 'CTGGGGATTTA' gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta > Still_hope_to_find_match.txt”.
+#Comment for exercise 6 by Chin San Loo
+#That paper has published their electrophoretic mobility shift assay-sequencing (EMSA-Seq) dataset, we could use 'grep' to check how much line or list of gene from "gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta" contain the NFKB canonical sequence (11mer forward and reverse complement) that obtained from their published EMSA-seq dataset.
+#####
+#Finding match between canonical nkfb 11mer forward and the fasta 
+grep -F -f nfkb_forward.txt gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta > Output_F.txt
+#Counting how many lines have matched
+wc -l Output_F.txt
+# Obtain 513 lines
+#####
+#Finding match between canonical nkfb 11mer reverse and the fasta
+grep -F -f nfkb_reverse.txt gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta > Output_R.txt
+#Counting how many lines have matched
+wc -l Output_F.txt
+# Obtain 400 lines
+
+# Total match is (513+400)= 913, Percentage of match is 913/2442=37.4%
 
  echo "Hello I am a message in standard out (stdout)"
  echo "Hello I am a message in standard error (stderr)" >&2
